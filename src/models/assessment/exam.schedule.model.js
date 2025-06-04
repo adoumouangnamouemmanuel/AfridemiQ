4ec6476451faa2717d2abeb14f4963c334e91d0e
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-04 21:24:42 +0000

    exam schedule module

---
+++ src/models/assessment/exam.schedule.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ExamScheduleSchema = new Schema(
+  {
+    examId: { type: Schema.Types.ObjectId, ref: "Exam", required: true },
+    subjectId: { type: Schema.Types.ObjectId, ref: "Subject", required: true },
+    series: String,
+    level: { type: String, required: true },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    duration: { type: Number, required: true },
+    location: String,
+    onlineLink: String,
+    notes: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = {
+  ExamSchedule: mongoose.model("ExamSchedule", ExamScheduleSchema),
+};
--- src/models/test.js
+++ src/models/test.js
@@ -241,20 +241,6 @@ const NoteSchema = new Schema({
   updatedAt: Date,
 }, { timestamps: true });
 
-const ExamScheduleSchema = new Schema({
-  examId: { type: Schema.Types.ObjectId, ref: 'Exam', required: true },
-  subjectId: { type: Schema.Types.ObjectId, ref: 'Subject', required: true },
-  series: String,
-  level: { type: String, required: true },
-  date: { type: Date, required: true },
-  time: { type: String, required: true },
-  duration: { type: Number, required: true },
-  location: String,
-  onlineLink: String,
-  notes: String,
-}, { timestamps: true });
-
-
 // Models
 module.exports = {
   Exercise: mongoose.model('Exercise', ExerciseSchema),
@@ -266,5 +252,4 @@ module.exports = {
   LeaderboardEntry: mongoose.model('LeaderboardEntry', LeaderboardEntrySchema),
   Mission: mongoose.model('Mission', MissionSchema),
   Note: mongoose.model('Note', NoteSchema),
-  ExamSchedule: mongoose.model('ExamSchedule', ExamScheduleSchema),
 };
