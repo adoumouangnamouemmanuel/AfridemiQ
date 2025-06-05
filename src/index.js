113f42a969f6c3ce6326498f8a8ebadbc8301388
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-05 18:50:41 +0000

    redis free version

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/user/user.route');
-const errorMiddleware = require('./middlewares/error.middleware');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user.route");
+const errorMiddleware = require("./middlewares/error.middleware");
+require("dotenv").config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 
+// Debug environment variables
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+// Validate MONGO_URI
+if (!process.env.MONGO_URI) {
+  console.error("Error: MONGO_URI is not defined in .env file");
+  process.exit(1);
+}
+
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 // Error handling middleware
 app.use(errorMiddleware);
