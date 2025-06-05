438412eaff46bd11dbadaa425d47f04a3aa39d98
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-05 16:26:58 +0000

    auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,12 @@
 const jwt = require("jsonwebtoken");
+const redis = require("redis");
 const UnauthorizedError = require("../errors/unauthorizedError");
+
+// Redis client
+const redisClient = redis.createClient({ url: process.env.REDIS_URL });
+redisClient
+  .connect()
+  .catch((err) => console.error("Redis connection error:", err));
 
 // Verify JWT token
 const authMiddleware = async (req, res, next) => {
@@ -9,6 +16,8 @@ const authMiddleware = async (req, res, next) => {
   }
   const token = authHeader.split(" ")[1];
   try {
+    const isBlacklisted = await redisClient.get(`blacklist:${token}`);
+    if (isBlacklisted) throw new UnauthorizedError("Token révoqué");
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { _id: payload.userId, role: payload.role };
     next();
