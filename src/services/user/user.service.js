e7cb77764822bc487a54935dd16442fd0f5b2dc2
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-05 15:35:53 +0000

    unauthorized error

--- src/services/user/user.service.js
+++ src/services/user/user.service.js
@@ -3,7 +3,7 @@ const { User } = require("../../modesls/index");
 const { generateToken } = require("../utils/jwt.utils");
 const BadRequestError = require("../../errors/badRequestError");
 const NotFoundError = require("../errors/notFoundError");
-const UnauthorizedError = require("../errors/unauthorizedError");
+const UnauthorizedError = require("../../errors/unauthorizedError")
 const ConflictError = require("../errors/conflictError");
 
 // Register a new user
