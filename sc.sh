f09ba695900355cd85dba65cc3699289ce35fdd8
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-07 00:48:53 +0000

    user is now okay

--- sc.sh
+++ sc.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-  curl -X PUT "http://localhost:3000/api/users/subscription" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODQzNmY5MDJjM2E2NDYzMzgyOWQxNjgiLCJyb2xlIjoic3R1ZGVudCIsImlhdCI6MTc0OTI1Mzg2MywiZXhwIjoxNzQ5MjU3NDYzfQ.FZzVSgDjMMv0hvEihFDSr0s79MfF_G-MljPJUdewHF4" \
-  -d '{
-    "type": "premium",
-    "startDate": "2024-03-20",
-    "expiresAt": "2024-04-20",
-    "features": ["unlimited_access", "priority_support"]
-  }'
+  curl -X GET "http://localhost:3000/api/users" \
+  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODQyZTk4ZjE1NTg5Y2Y0OWNjZGY5MTYiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3NDkyNTcyNDYsImV4cCI6MTc0OTI2MDg0Nn0.0j_E9dqQlt2m-_cr2eUI41eVYYzgBgcRNjVUqMOO-1s"
