5cae8bedc769bff1b47a015f4ef0e4c3b2a13d3e
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-11 14:31:25 +0000

    exported loader

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -1,4 +1,5 @@
 export { default as CustomButton } from "./CustomButton";
 export { default as CustomInput } from "./CustomInput";
 export { default as Divider } from "./Divider";
+export { default as Loader } from "./Loader";
 export { default as SocialButton } from "./SocialButton";
