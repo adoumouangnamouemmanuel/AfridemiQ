393d7db9781453e79e38193ae3d311d765a6caf3
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-12 22:31:57 +0000

    index exporting custom picker

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -1,5 +1,6 @@
 export { default as CustomButton } from "./CustomButton";
 export { default as CustomInput } from "./CustomInput";
+export { default as CustomPicker } from "./CustomPicker";
 export { default as Divider } from "./Divider";
 export { default as Loader } from "./Loader";
 export { default as SocialButton } from "./SocialButton";
