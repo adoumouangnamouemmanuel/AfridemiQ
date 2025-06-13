5850d125db0720af4cb2caccd07e8c79310c4a95
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-13 19:19:24 +0000

    added status bar

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Stack } from "expo-router"
-import { StatusBar } from "expo-status-bar"
+import { StatusBar} from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { ThemeProvider } from "../src/utils/ThemeContext"
@@ -39,10 +39,10 @@ export default function RootLayout() {
               <Stack.Screen name="(tabs)" />
               <Stack.Screen name="(routes)" />
             </Stack>
-            <StatusBar style="auto" />
+            <StatusBar backgroundColor="#007AFF" barStyle="light-content" />
           </UserProvider>
         </ThemeProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
-  )
+  );
 }
