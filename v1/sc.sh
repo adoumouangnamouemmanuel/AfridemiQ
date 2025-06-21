6a43be65033684520902a0de45c948f2cbfa035b
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-21 20:04:55 +0000

    new scripts

--- v1/sc.sh
+++ v1/sc.sh
@@ -1,5 +1,8 @@
-YOUR_ADMIN_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODU2OWM2Mzc0MzYxMmJjOGExMWRmOWYiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3NTA1MjM0NjQsImV4cCI6MTc1MDUyNzA2NH0.C8EgmPAeloGkcKB-JZ6DHgp-jBvlDFYWIhEamdrVuDo"
+admin_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODU2OWM2Mzc0MzYxMmJjOGExMWRmOWYiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3NTA1MzMzNTcsImV4cCI6MTc1MDUzNjk1N30.w28pgzw2R1bgpOQAxWX7Qwb9L6l5RVG955gn5_9Gl0U"
+math="6856e110aff9659b6e3ec887"
 
-curl -X DELETE http://localhost:3000/api/v1/subjects/6856e19caff9659b6e3ec890 \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $YOUR_ADMIN_TOKEN" \
+# Create UTF-8 encoded file and reference it
+curl -X POST http://localhost:3000/api/v1/topics \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -H "Authorization: Bearer $admin_token" \
+  -d @topic-french.json
