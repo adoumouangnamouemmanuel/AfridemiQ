81b72d0b0d3972dcec7927912e92f4533597d5f7
Adoum Ouang-namou Emmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-22 12:01:59 +0000

    v1: layout

---
+++ v1/app/_layout.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { Stack } from "expo-router"
+import { StatusBar} from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import { ThemeProvider } from "../src/utils/ThemeContext"
+import { UserProvider } from "../src/utils/UserContext"
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
+import { useEffect } from "react"
+
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Inter-Regular": require("../src/assets/fonts/Inter_28pt-Regular.ttf"),
+    "Inter-Bold": require("../src/assets/fonts/Inter_28pt-Bold.ttf"),
+    "Inter-SemiBold": require("../src/assets/fonts/Inter_28pt-SemiBold.ttf"),
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <ThemeProvider>
+          <UserProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="index" />
+              <Stack.Screen name="(tabs)" />
+              <Stack.Screen name="(routes)" />
+            </Stack>
+            <StatusBar backgroundColor="#007AFF" barStyle="light-content" />
+          </UserProvider>
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
